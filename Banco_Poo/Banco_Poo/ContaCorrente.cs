fa721325ba20018f49f99926289b4e9194583511
Isabela-Ribeiro <72897583+Isabela-Ribeiro@users.noreply.github.com>
2021-02-17 08:24:35 -0300

    Add files via upload
    Exercício do banco para fazer transferência, saque, deposito, e consultar

---
+++ Banco_Poo/Banco_Poo/ContaCorrente.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco_Poo
+{
+     class ContaCorrente
+    {
+        
+        private double _agencia;
+        private double _saldo;
+        public string Cpf { get; set; }
+        public string Nome { get; set; }
+        public string Rua { get; set; }
+        public int NumeroCasa { get; set; }
+        public string Cidade { get; set; }
+        public double Numero { get; set; }
+
+        public double Agencia 
+        { 
+            get
+            {
+                return _agencia;
+            }
+            set 
+            {
+                if (value <= 0)
+                {
+                    return;
+                }
+                _agencia = value;
+            } 
+        }
+        
+        public double Saldo 
+        {
+            get 
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                _saldo = value;
+            }
+        }
+        
+        public void Depositar()
+        {
+            double valor;
+            Console.WriteLine("Digite quanto irá depositar:");
+            valor = int.Parse(Console.ReadLine());
+            this._saldo += valor;
+            Console.WriteLine("Deposito realizado com sucesso!");
+        }
+        public bool Sacar()
+        {
+            double valor;
+            Console.WriteLine("Digite quanto irá sacar:");
+            valor = int.Parse(Console.ReadLine());
+            if (this._saldo < valor)
+            {
+                Console.WriteLine("Valor maior do que disponivel!");
+                return false;
+                
+            }
+
+            this._saldo -= valor;
+            Console.WriteLine("Saque realizado com sucesso!");
+            return true;
+        }
+        public bool Tranferir()
+        {
+            Console.WriteLine("Conta Existente ag:210 , conta:12345");
+            double valor;  
+            Console.WriteLine("Digite a AGENCIA para:");
+            int agen = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a Conta para:");
+            int conta = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("valor para a tranferencia");
+            valor = double.Parse(Console.ReadLine());
+
+            if (agen == 210 && conta == 12345)
+            {
+                if (this._saldo < valor)
+                {
+                    Console.WriteLine("valor maior que o saldo");
+                    return false;
+                }
+                this._saldo -= valor;
+                Console.WriteLine("Tranferencia realizado com sucesso!");
+
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Conta inexistente!");
+            }
+            return false;
+            
+        }
+    }
+}
---
+++ Banco_Poo/Banco_Poo/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco_Poo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            ContaCorrente clienteConta = new ContaCorrente();
+            int id=0;
+           
+            int opc;
+            do
+            {
+                Console.WriteLine("\tMENU PRINCIPAL");
+                Console.WriteLine("1-CADASTRAR\n2-DEPOSITO\n3-SAQUE\n4-TRANFERENCIA\n5-CONSULTAR SALDO\n0-SAIR ");
+                Console.WriteLine("Digite a opção desejada: ");
+                opc = int.Parse(Console.ReadLine());
+                Console.Clear();
+                switch (opc)
+                {
+                    case 1:
+                        Console.WriteLine("\tCasdastrar Novo Titular");
+                       
+                        Console.Write("Nome : ");
+                        clienteConta.Nome = Console.ReadLine();
+                        Console.Write("Informe o CPF sem os ponto : ");
+                        clienteConta.Cpf = Console.ReadLine();
+                        Console.Write("Rua :");
+                        clienteConta.Rua = Console.ReadLine();
+                        Console.Write("Numero da rsidencia:");
+                        clienteConta.NumeroCasa = int.Parse(Console.ReadLine());
+                        Console.Write("Cidade :");
+                        clienteConta.Cidade = Console.ReadLine();
+                        Console.Write("Agencia :");
+                        clienteConta.Agencia = int.Parse(Console.ReadLine());
+                        Console.Write("conta : ");
+                        clienteConta.Numero = int.Parse(Console.ReadLine());
+                        Console.Clear();
+
+                        break;
+                    case 2:
+                        Console.WriteLine("DEPOSITAR");
+                        
+                        clienteConta.Depositar();
+                        
+                        break;
+                    case 3:
+                        Console.WriteLine("SAQUE");
+                        clienteConta.Sacar();
+                        break;
+                    case 4:
+                        Console.WriteLine("TRANSFERIR");
+                        clienteConta.Tranferir();
+                        break;
+                    case 5:
+                        Console.WriteLine("SALDO");
+                        Console.WriteLine("Seu saldo é : " +clienteConta.Saldo);
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        Console.WriteLine("Opção Invalida!");
+                        break;
+                }
+            }while(opc != 0);
+            Console.ReadKey();
+
+
+        }
+       
+    }
+}
